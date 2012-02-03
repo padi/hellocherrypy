bfc6f83dc3a33672d1f9e75aec44b18f48ba1857
Padi Ignacio <marcrendlignacio@gmail.com>
2012-02-03 18:22:40 +0800

    Simple program exposing objects

---
+++ exposing.py
@@ -0,0 +1,21 @@
+"""
+Tutorial - Multiple methods
+
+This tutorial shows you how to link to other methods of your request
+handler.
+"""
+
+import cherrypy
+
+class HelloWorld:
+    
+    def index(self):
+        # Let's link to another method here.
+        return 'We have an <a href="showMessage">important message</a> for you!'
+    index.exposed = True
+    
+    def showMessage(self):
+        # Here's the important message!
+        return "Hello world!"
+    showMessage.exposed = True
+cherrypy.quickstart(HelloWorld())
