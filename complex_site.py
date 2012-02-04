dd9841ac41dddf60e9050bd3bfc25d1cedf63345
Padi Ignacio <marcrendlignacio@gmail.com>
2012-02-04 10:28:39 +0800

    Added additional pages for complex_site.

--- complex_site.py
+++ complex_site.py
@@ -12,5 +12,49 @@ class HomePage:
             </ul>'''
     index.exposed = True
 
+class JokePage:
+    def index(self):
+        return '''
+        <p>"In Python, how do you create a string of random
+        characters?" -- "Read a Perl file"</p>
+        <p>[<a href="../">Return</a>]</p>'''
+    index.exposed = True
+
+class LinksPage:
+    def __init__(self):
+        self.extra = ExtraLinksPage()
+
+    def index(self):
+        return '''
+            <p>Here are some useful links:</p>
+
+            <ul>
+                <li><a href="http://www.cherrypy.org">The CherryPy Homepage</a></li>
+                <li><a href="http://www.python.org">The Python Homepage</a></li>
+            </ul>
+
+            <p>You can check out some extra useful
+            links <a href="./extra/">here</a>.</p>
+
+            <p>[<a href="../">Return</a>]</p>
+        '''
+    index.exposed = True
+
+class ExtraLinksPage:
+    def index(self):
+        return '''
+            <p>Here are some extra useful links:</p>
+
+            <ul>
+                <li><a href="http://def.icio.us">del.icio.us</a></li>
+                <li><a href="http://www.mornography.de">Hendrik's weblog</a></li>
+            </ul>
+
+            <p>[<a href="../">Return to links page</a>]</p>'''
+    index.exposed = True
+
 root = HomePage()
+root.joke = JokePage()
+root.links = LinksPage()
+
 cherrypy.quickstart(root)
